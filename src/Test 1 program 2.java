246957c261cbcee8d9dc5f04e05caff18e2118e4
wandrics <shahebaz.wandrick@hotmail.com>
2025-06-23 13:08:56 +0300

    Add files via upload

---
+++ src/Test 1 program 2.java
@@ -0,0 +1,23 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class Main {
+    public static void main(String[] args) {
+        int a = 10; //declaring an int variable a
+        int b = 30; //declaring second int variable b
+        
+        System.out.println("Arithematic operation");
+        System.out.println("Addition of a and b=" + (a+b)); //addition of two number
+        System.out.println("subtraction of a and b=" + (a-b));//subtraction of two number
+        System.out.println("Multiplication of a and b=" + (a*b));//multiplication of two number
+        System.out.println("a/b=" + (a/b));//divsion of two number
+        System.out.println("a%b=" + (a%b));//subtraction of two number
+        
+        System.out.println("Comparision operation");
+        System.out.println("a == b : " + (a == b)); // false
+        System.out.println("a != b : " + (a != b)); // true
+        System.out.println("a > b  : " + (a > b));  // false
+        System.out.println("a < b  : " + (a < b));  // true
+        System.out.println("a >= b : " + (a >= b)); // false
+        System.out.println("a <= b : " + (a <= b)); // 
+        }
